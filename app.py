93ada6e46621ac0122792c090eefe36087f8a142
Fauzan Mohammed <mohammedfauzan44@gmail.com>
2022-03-22 13:53:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,35 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
+from flask_cors import cross_origin
+import sklearn
 import pickle
+import pandas as pd
+
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
+@cross_origin()
 def home():
     return render_template('index.html')
 
-
-@app.route('/predict',methods=['POST'])
+@app.route("/predict", methods = ["GET", "POST"])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    features = [int(x) for x in request.form.values()]
-    final_features = [np.array(features)]
-    prediction = model.predict(final_features)
+    if request.method == "POST":
+        features = [int(x) for x in request.form.values()]
+        final_features = [np.array(features)]
+        prediction = model.predict(final_features)
 
-    output = prediction
-    final=[]
-    for val in output:
+        output = prediction
+        final=[]
+        for val in output:
         if val=2:
             final.append("Benign")
         else: val=4:
             final.append("Cancerous")
 
-    return render_template('index.html', prediction_text='Your Cells are{}'.format(final))
+        return render_template('index.html', prediction_text='Your Cells are{}'.format(final))
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
